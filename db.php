40a1ab951499ca5e5e3747906c7a09a92891912e
Alexandros Tzamalis <alextzamalis16@gmail.com>
2026-01-15 21:30:42 +0200

    Added Comments db.php

--- db.php
+++ db.php
@@ -1,10 +1,19 @@
 <?php
+
+/*
+ * Ρυθμίσεις της βασησ δεδομένων
+*/
+
+// Στοιχεία Συνδεσης
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "university_db";
 
+// Δημιουργια συνδεσης
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Σταματαει την εκτέλεση και εμφανίζει το error 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
